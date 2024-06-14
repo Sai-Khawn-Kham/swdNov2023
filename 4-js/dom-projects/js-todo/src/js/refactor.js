2f2143318883a21864daabfe9360b1cfc2d3ec38
Sai Khawn Kham <khawnkham222@gmail.com>
2024-06-14 15:58:13 +0630

    DOM node finish

--- 4-js/dom-projects/js-todo/src/js/refactor.js
+++ 4-js/dom-projects/js-todo/src/js/refactor.js
@@ -5,6 +5,9 @@ const listGroup = document.querySelector("#listGroup");
 const allTask = document.querySelector("#allTask");
 const doneTask = document.querySelector("#doneTask");
 // let count = 1;
+const deleteAll = document.querySelector("#deleteAll");
+const doneAll = document.querySelector("#doneAll");
+const listTemplate = document.querySelector("#listTemplate");
 
 // action (business logic)
 textInput.focus();
@@ -18,30 +21,9 @@ const updateDoneTask = () => {
     doneTask.innerText = doneList;
 };
 const createNewList = (currentTask) => {
-    const list = document.createElement("div");
-    list.id = "list" + Date.now();
-    // list.id = "list" + count++;
-    list.classList.add("list");
-    list.innerHTML = `
-    <div class="flex animate__zoomIn animate__animated justify-between border-2 border-stone-950 mb-3 p-3">
-        <div class="flex gap-3">
-            <input type="checkbox" class="checkInput disabled:opacity-30 accent-stone-950">
-            <p class="font-mono my-auto listPara">${currentTask}</p>
-        </div>
-        <div class="flex gap-2 duration-500 twoBtn">
-            <button id="" class="editBtn active:scale-75 duration-200 border-2 disabled:opacity-30 border-stone-950 p-1 rounded-md">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 pointer-events-none">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
-                </svg>
-            </button>
-            <button id="" class="deleteBtn active:scale-75 duration-200 border-2 border-stone-950 p-1 rounded-md">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 pointer-events-none" >
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-                </svg>
-            </button>
-        </div>
-    </div>
-    `;
+    const list = listTemplate.content.cloneNode(true);
+    list.querySelector(".list").id = "list" + Date.now();
+    list.querySelector(".listPara").innerText = currentTask;
     return list;
 };
 
@@ -55,6 +37,7 @@ const checkList = (listId) => {
     const currentList = document.querySelector(`#${listId}`);
     const checkInput = currentList.querySelector(".checkInput");
     const listPara = currentList.querySelector(".listPara");
+    const editBtn = currentList.querySelector(".editBtn");
     updateDoneTask();
     listPara.classList.toggle("line-through");
     currentList.classList.toggle("opacity-70");
@@ -117,16 +100,16 @@ const deleteList = (listId) => {
 const addBtnHandler = (e) => {
     // console.log(textInput.value.trim() ? true : false);
     // console.log(textInput.value.trim());
-        if (textInput.value.trim() != "") {
-            addList(textInput.value);
-        } else {
-            alert("Input task");
+    if (textInput.value.trim() != "") {
+        addList(textInput.value);
+    } else {
+        alert("Input task");
     }
 };
 const textInputHandler = (e) => {
     // console.log(textInput.value.trim() ? true : false);
     // console.log(textInput.value.trim());
-    if(e.key == "Enter"){
+    if (e.key == "Enter") {
         if (textInput.value.trim() != "") {
             addList(textInput.value);
         } else {
@@ -146,8 +129,25 @@ const listGroupHandler = (e) => {
         deleteList(e.target.closest(".list").id);
     }
 };
+const deleteAllHandler = (e) => {
+    if (confirm("are you sure to remove all tasks?")) {
+        const allList = listGroup.querySelectorAll(".list");
+        allList.forEach((list) => list.remove());
+    }
+};
+const doneAllHandler = (e) => {
+    if (confirm("are you sure to done all task?")) {
+        const allList = listGroup.querySelectorAll(".list");
+        allList.forEach((list) => {
+            checkList(list.id);
+            list.querySelector(".checkInput").checked = true;
+        });
+    }
+};
 
 // listeners
 addBtn.addEventListener("click", addBtnHandler);
 textInput.addEventListener("keyup", textInputHandler);
 listGroup.addEventListener("click", listGroupHandler);
+deleteAll.addEventListener("click", deleteAllHandler);
+doneAll.addEventListener("click", doneAllHandler);
